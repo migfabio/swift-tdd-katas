a6e47fda1747983cd7afdb2eab7ab26146e0e12c
Fabio Mignogna <fabio@expensify.com>
2019-08-24 17:04:08 +0100

    greet function trim the name

--- Greeter/GreeterTests/GreeterTests.swift
+++ Greeter/GreeterTests/GreeterTests.swift
@@ -11,15 +11,20 @@ import XCTest
 
 class Greeter {
     func greet(_ name: String) -> String {
-        return "Hello \(name)"
+        return "Hello \(name.trimmingCharacters(in: .whitespaces))"
     }
 }
 
 class GreeterTests: XCTestCase {
 
-    func test_greet_withNameAsInput_ShoulReturnHelloName() {
+    func test_greet_withNameAsInput_shoulReturnHelloName() {
         let sut = Greeter()
         XCTAssertEqual(sut.greet("Fabio"), "Hello Fabio")
         XCTAssertEqual(sut.greet("Jenny"), "Hello Jenny")
+    }
+
+    func test_greet_withNameWithWhitespaces_shouldReturnHelloNameWithoutWhitespaces() {
+        let sut = Greeter()
+        XCTAssertEqual(sut.greet("  Fabio  "), "Hello Fabio")
     }
 }
