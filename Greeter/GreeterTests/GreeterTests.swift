42d4f75fe40a670deddcd601a1af893d871d30c8
Fabio Mignogna <fabio@expensify.com>
2019-08-25 14:47:07 +0100

    greet returns Good night <name> when the time is 22:00-06:00

--- Greeter/GreeterTests/GreeterTests.swift
+++ Greeter/GreeterTests/GreeterTests.swift
@@ -10,6 +10,36 @@ import Foundation
 import XCTest
 
 class Greeter {
+    private var today: Date {
+        let calendar = Calendar(identifier: .gregorian)
+        return calendar.startOfDay(for: Date())
+    }
+    
+    private var todaySixAm: Date {
+        let calendar = Calendar(identifier: .gregorian)
+        return calendar.date(bySetting: .hour, value: 06, of: today)!
+    }
+    
+    private var todayNoon: Date {
+        let calendar = Calendar(identifier: .gregorian)
+        return calendar.date(bySetting: .hour, value: 12, of: today)!
+    }
+    
+    private var todaySixPm: Date {
+        let calendar = Calendar(identifier: .gregorian)
+        return calendar.date(bySetting: .hour, value: 18, of: today)!
+    }
+    
+    private var todayTenPm: Date {
+        let calendar = Calendar(identifier: .gregorian)
+        return calendar.date(bySetting: .hour, value: 22, of: today)!
+    }
+    
+    private var tomorrowSixAm: Date {
+        let calendar = Calendar(identifier: .gregorian)
+        return calendar.date(byAdding: .day, value: 1, to: todaySixAm)!
+    }
+    
     private let date: Date?
     
     init(date: Date? = nil) {
@@ -19,12 +49,12 @@ class Greeter {
     func greet(_ name: String) -> String {
         var greetingMessage = "Hello"
         if let date = date {
-            let calendar = Calendar(identifier: .gregorian)
-            let hour = calendar.component(.hour, from: date)
-            if hour >= 6 && hour <= 12 {
+            if date >= todaySixAm && date < todayNoon {
                 greetingMessage = "Good morning"
-            } else if hour >= 18 && hour <= 22 {
+            } else if date >= todaySixPm && date < todayTenPm {
                 greetingMessage = "Good evening"
+            } else if date >= todayTenPm && date < tomorrowSixAm {
+                greetingMessage = "Good night"
             }
         }
         
@@ -53,14 +83,19 @@ class GreeterTests: XCTestCase {
         XCTAssertEqual(makeSUT().greet("fabio"), "Hello Fabio")
     }
     
-    func test_greet_withNameAsInput_whenTimeIsBetween06amToNoon_shouldReturnGoodMorningName() {
+    func test_greet_withNameAsInput_whenTimeIsBetween06amIncludedToNoonExcluded_shouldReturnGoodMorningName() {
         XCTAssertEqual(makeSUT(with: .sixAM).greet("Fabio"), "Good morning Fabio")
-        XCTAssertEqual(makeSUT(with: .noon).greet("Fabio"), "Good morning Fabio")
+        XCTAssertEqual(makeSUT(with: .customHourAndMinute(hour: 11, minute: 59)).greet("Fabio"), "Good morning Fabio")
     }
     
-    func test_greet_withNameAsInput_whenTimeIsBetween06pmTo10pm_shouldReturnGoodEveningName() {
+    func test_greet_withNameAsInput_whenTimeIsBetween06pmIncludedTo10pmExcluded_shouldReturnGoodEveningName() {
         XCTAssertEqual(makeSUT(with: .sixPM).greet("Fabio"), "Good evening Fabio")
-        XCTAssertEqual(makeSUT(with: .tenPM).greet("Fabio"), "Good evening Fabio")
+        XCTAssertEqual(makeSUT(with: .customHourAndMinute(hour: 21, minute: 59)).greet("Fabio"), "Good evening Fabio")
+    }
+    
+    func test_greet_withNameAsInput_whenTimeIsBetween10pmIncludedTo06amExcluded_shouldReturnGoodNightName() {
+        XCTAssertEqual(makeSUT(with: .tenPM).greet("Fabio"), "Good night Fabio")
+        XCTAssertEqual(makeSUT(with: .customHourAndMinute(hour: 5, minute: 59, nextDay: true)).greet("Fabio"), "Good night Fabio")
     }
     
     // MARK: - Helper
@@ -74,18 +109,27 @@ class GreeterTests: XCTestCase {
         case noon
         case sixPM
         case tenPM
+        case customHourAndMinute(hour: Int, minute: Int, nextDay: Bool = false)
+        
+        private var today: Date {
+            let calendar = Calendar(identifier: .gregorian)
+            return calendar.startOfDay(for: Date())
+        }
         
         var date: Date? {
             let calendar = Calendar(identifier: .gregorian)
             switch self {
             case .sixAM:
-                return calendar.date(bySetting: .hour, value: 06, of: Date())
+                return calendar.date(bySetting: .hour, value: 06, of: today)
             case .noon:
-                return calendar.date(bySetting: .hour, value: 12, of: Date())
+                return calendar.date(bySetting: .hour, value: 12, of: today)
             case .sixPM:
-                return calendar.date(bySetting: .hour, value: 18, of: Date())
+                return calendar.date(bySetting: .hour, value: 18, of: today)
             case .tenPM:
-                return calendar.date(bySetting: .hour, value: 22, of: Date())
+                return calendar.date(bySetting: .hour, value: 22, of: today)
+            case .customHourAndMinute(let hour, let minute, let nextDay):
+                let customDate = calendar.date(bySettingHour: hour, minute: minute, second: 0, of: today)
+                return nextDay ? calendar.date(byAdding: .day, value: 1, to: customDate!) : customDate
             }
         }
     }
