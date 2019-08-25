2241e515e0998d6162d76f9dcadbf4e226ed2dd8
Fabio Mignogna <fabio@expensify.com>
2019-08-25 13:23:01 +0100

    greet returns Good morning <name> when the time is 06:00-12:00

--- Greeter/GreeterTests/GreeterTests.swift
+++ Greeter/GreeterTests/GreeterTests.swift
@@ -10,8 +10,23 @@ import Foundation
 import XCTest
 
 class Greeter {
+    private let date: Date?
+    
+    init(date: Date? = nil) {
+        self.date = date
+    }
+    
     func greet(_ name: String) -> String {
-        return "Hello \(name.trimmingCharacters(in: .whitespaces).capitalizeFirstLetter())"
+        var greetingMessage = "Hello"
+        if let date = date {
+            let calendar = Calendar(identifier: .gregorian)
+            let hour = calendar.component(.hour, from: date)
+            if hour >= 6 && hour <= 12 {
+                greetingMessage = "Good morning"
+            }
+        }
+        
+        return "\(greetingMessage) \(name.trimmingCharacters(in: .whitespaces).capitalizeFirstLetter())"
     }
 }
 
@@ -34,7 +49,20 @@ class GreeterTests: XCTestCase {
         XCTAssertEqual(sut.greet("  Fabio  "), "Hello Fabio")
     }
     
-    func test_great_withNameAsInput_shouldCapitalizeTheFirstLetterOfTheName() {
+    func test_greet_withNameAsInput_shouldCapitalizeTheFirstLetterOfTheName() {
         XCTAssertEqual(sut.greet("fabio"), "Hello Fabio")
+    }
+    
+    func test_greet_withNameAsInput_whenTimeIsBetween06amTo12pm_shouldReturnGoodMorningName() {
+        let calendar = Calendar(identifier: .gregorian)
+        let sixAm = calendar.date(bySetting: .hour, value: 06, of: Date())
+        var sut = Greeter(date: sixAm)
+        
+        XCTAssertEqual(sut.greet("Fabio"), "Good morning Fabio")
+        
+        let twelvePm = calendar.date(bySetting: .hour, value: 12, of: Date())
+        sut = Greeter(date: twelvePm)
+        
+        XCTAssertEqual(sut.greet("Fabio"), "Good morning Fabio")
     }
 }
