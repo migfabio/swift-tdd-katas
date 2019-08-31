3238a5593a24ef87ced5c3d96b5ac86e8c603da8
Fabio Mignogna <fabio@expensify.com>
2019-08-31 14:16:22 +0100

    add with negative numbers throw an exception with the message

--- StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
+++ StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
@@ -8,10 +8,26 @@
 
 import XCTest
 
+enum StringCalculatorError: Error, LocalizedError {
+    case nonNegativeNumber(numbers: [Int])
+    
+    var errorDescription: String? {
+        switch self {
+        case .nonNegativeNumber(let numbers):
+            return "Negatives not allowed: \(numbers.map { String($0) }.joined(separator: ","))"
+        }
+    }
+}
+
 class StringCalculator {
-    func add(_ input: String) -> Int {
+    
+    func add(_ input: String) throws -> Int {
         let intValues = input.split { $0 == "," || $0 == "\n" }
             .map { Int($0.trimmingCharacters(in: .whitespaces)) ?? 0 }
+        let negativeNumbers = intValues.filter({ $0 < 0 })
+        guard negativeNumbers.isEmpty else {
+            throw StringCalculatorError.nonNegativeNumber(numbers: negativeNumbers)
+        }
         return intValues.reduce(0, +)
     }
 }
@@ -21,28 +37,39 @@ class StringCalculatorTests: XCTestCase {
     private let sut = StringCalculator()
     
     func test_add_whenInputIsEmptyString_shouldReturnZero() {
-        XCTAssertEqual(sut.add(""), 0)
-        XCTAssertEqual(sut.add("  "), 0)
+        XCTAssertEqual(try! sut.add(""), 0)
+        XCTAssertEqual(try! sut.add("  "), 0)
     }
     
     func test_add_whenInputIsSingleNumber_shouldReturnValue() {
-        XCTAssertEqual(sut.add("1"), 1)
-        XCTAssertEqual(sut.add("2"), 2)
-        XCTAssertEqual(sut.add("3   "), 3)
+        XCTAssertEqual(try! sut.add("1"), 1)
+        XCTAssertEqual(try! sut.add("2"), 2)
+        XCTAssertEqual(try! sut.add("3   "), 3)
     }
     
     func test_add_whenInputIsTwoNumberCommaDelimited_shouldReturnSum() {
-        XCTAssertEqual(sut.add("1,2"), 3)
-        XCTAssertEqual(sut.add("2  ,  3"), 5)
+        XCTAssertEqual(try! sut.add("1,2"), 3)
+        XCTAssertEqual(try! sut.add("2  ,  3"), 5)
     }
     
     func test_add_whenInputIsTwoNumberNewLineDelimited_shouldReturnSum() {
-        XCTAssertEqual(sut.add("1\n2"), 3)
-        XCTAssertEqual(sut.add("1  \n  2"), 3)
+        XCTAssertEqual(try! sut.add("1\n2"), 3)
+        XCTAssertEqual(try! sut.add("1  \n  2"), 3)
     }
     
     func test_add_whenInputIsThreeNumberDelimitedEitherWay_shouldReturnSum() {
-        XCTAssertEqual(sut.add("1\n2,3\n4"), 10)
-        XCTAssertEqual(sut.add(" 1 \n 2 , 3 \n 4 "), 10)
+        XCTAssertEqual(try! sut.add("1\n2,3\n4"), 10)
+        XCTAssertEqual(try! sut.add(" 1 \n 2 , 3 \n 4 "), 10)
+    }
+        
+    func test_add_whenInputAreNegativeNumbers_shouldThrownNonNegativeNumberException() {
+        XCTAssertThrowsError(try sut.add("-1,2,-3")) { (error) in
+            guard case StringCalculatorError.nonNegativeNumber(let values) = error else {
+                return XCTFail("StringCalculator does not thrown an exception with negative numbers")
+            }
+
+            XCTAssertEqual(values, [-1, -3])
+            XCTAssertEqual(error.localizedDescription, "Negatives not allowed: -1,-3")
+        }
     }
 }
