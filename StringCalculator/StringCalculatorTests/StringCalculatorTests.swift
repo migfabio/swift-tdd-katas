9339f3f04dc86f6b1f3a965b8da94b8e04da5141
Fabio Mignogna <fabio@expensify.com>
2019-08-31 10:41:01 +0100

    add with a single number returns the value

--- StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
+++ StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
@@ -10,15 +10,20 @@ import XCTest
 
 class StringCalculator {
     func add(_ input: String) -> Int {
-        return 0
+        return Int(input) ?? 0
     }
 }
 
 class StringCalculatorTests: XCTestCase {
     
+    private let sut = StringCalculator()
+    
     func test_add_whenInputIsEmptyString_shouldReturnZero() {
-        let sut = StringCalculator()
-        
         XCTAssertEqual(sut.add(""), 0)
+    }
+    
+    func test_add_whenInputIsSingleNumber_shouldReturnValue() {
+        XCTAssertEqual(sut.add("1"), 1)
+        XCTAssertEqual(sut.add("2"), 2)
     }
 }
