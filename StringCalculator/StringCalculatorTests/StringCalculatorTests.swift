9f431530c442783da017907e31a8d77ad59710ea
Fabio Mignogna <fabio@expensify.com>
2019-08-31 10:47:16 +0100

    add with two numbers, comma delimited, returns the sum

--- StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
+++ StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
@@ -10,7 +10,11 @@ import XCTest
 
 class StringCalculator {
     func add(_ input: String) -> Int {
-        return Int(input) ?? 0
+        let values = input.split(separator: ",")
+        let sum = values.reduce(0) { (acc, value) -> Int in
+            return acc + (Int(value) ?? 0)
+        }
+        return sum
     }
 }
 
@@ -25,5 +29,9 @@ class StringCalculatorTests: XCTestCase {
     func test_add_whenInputIsSingleNumber_shouldReturnValue() {
         XCTAssertEqual(sut.add("1"), 1)
         XCTAssertEqual(sut.add("2"), 2)
+    }
+    
+    func test_add_whenInputIsTwoNumberCommaDelimited_shouldReturnSum() {
+        XCTAssertEqual(sut.add("1,2"), 3)
     }
 }
