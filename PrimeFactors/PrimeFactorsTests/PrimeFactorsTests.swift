26227cf2bd23ad0488979f391659c82d5eb303f1
Fabio Mignogna <fabio@expensify.com>
2019-09-07 20:57:45 +0100

    given 2 should return [2]

--- PrimeFactors/PrimeFactorsTests/PrimeFactorsTests.swift
+++ PrimeFactors/PrimeFactorsTests/PrimeFactorsTests.swift
@@ -10,6 +10,9 @@ import XCTest
 
 class PrimeFactors {
     func generate(_ input: Int) -> [Int] {
+        if input > 1 {
+            return [2]
+        }
         return []
     }
 }
@@ -19,5 +22,10 @@ class PrimeFactorsTests: XCTestCase {
     func test_generate_with1AsInput_shouldReturnAnEmptyArray() {
         let sut = PrimeFactors()
         XCTAssertEqual(sut.generate(1), [])
+    }
+    
+    func test_generate_with2AsInput_shouldReturn_2() {
+        let sut = PrimeFactors()
+        XCTAssertEqual(sut.generate(2), [2])
     }
 }
